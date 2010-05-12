649f9d56836d3df3ac6ccff3ce6fa747ced1e7f0
Heather Turner <ht@heatherturner.net>
2010-05-12 12:50:07 +0000

    predict methods now handle reduced data sets and `missing ' random effects
    git-svn-id: file:///home/hturner/BradleyTerry2@71 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/predict.BTm.R
+++ R/predict.BTm.R
@@ -1,18 +1,41 @@
-predict.BTm <- function (object, newdata = NULL, type = c("link", "response",
-    "terms"), se.fit = FALSE, dispersion = NULL, terms = NULL,
-    na.action = na.pass, ...) {
+predict.BTm <- function (object, newdata = NULL, level = 0,
+                         type = c("link", "response", "terms"), se.fit = FALSE,
+                         dispersion = NULL, terms = NULL,
+                         na.action = na.pass, ...) {
     if (!is.null(newdata)) {
         ## need to define X so will work with model terms
-        setup <- match(c("outcome", "player1", "player2", "formula", "id",
+        setup <- match(c("player1", "player2", "formula", "id",
                         "separate.ability", "refcat", "weights",
                         "subset", "offset", "contrasts"), names(object$call), 0L)
         setup <- as.call(c(quote(BradleyTerry2:::BTm.setup),
                            as.list(object$call)[setup],
                            list(data = newdata)))
         setup <- eval(setup, environment(object$formula))
+        if (ncol(setup$X) != length(fixef(object))){
+            ## will be due to separate abilities - else error by now
+            X <- matrix(0, nrow(setup$X), length(fixef(object)),
+                        dimnames = list(rownames(setup$X),
+                        names(fixef(object))))
+            X[, colnames(setup$X)] <- setup$X
+            setup$X <- X
+        }
         newdata <- data.frame(X = setup$X[,1])
         newdata$X <- setup$X # only need X for prediction (not Y)
-        return(NextMethod())
+        if (1 %in% level && ncol(setup$random) != length(ranef(object))){
+            ## expand to give col for every random effect
+            Z <- matrix(0, nrow(setup$random), length(ranef(object)),
+                        dimnames = list(rownames(setup$random),
+                        colnames(object$random))) #ranef need names!!
+            ## set to NA for contests with new players
+            miss <- !colnames(setup$random) %in% colnames(Z)
+            Z[, colnames(setup$random)[!miss]] <- setup$random[,!miss]
+            if (any(miss)) {
+                miss <- rowSums(setup$random[, miss, drop = FALSE] != 0) > 0
+                Z[miss,] <- NA
+            }
+            setup$random <- Z
+        }
+        return(NextMethod(newrandom = setup$random))
     }
     else
         NextMethod()
--- R/predict.glmmPQL.R
+++ R/predict.glmmPQL.R
@@ -1,8 +1,13 @@
-predict.glmmPQL <- function (object, newdata = NULL, newrandom = NULL, level = 1,
-                             type = c("link", "response", "terms"),
+predict.glmmPQL <- function (object, newdata = NULL, newrandom = NULL,
+                             level = 1, type = c("link", "response", "terms"),
                              se.fit = FALSE, dispersion = NULL, terms = NULL,
                              na.action = na.pass, ...) {
-    if (object$sigma == 0 || level == 0) return(NextMethod())
+    if (object$sigma == 0 || type == "terms") return(NextMethod())
+    if (0 %in% level) {
+        if (!is.null(newdata)) lev0 <- NextMethod()
+        else lev0 <- NextMethod("predict", object, newdata = object$data)
+        if (identical(level, 0)) return(lev0)
+    }
     type <- match.arg(type)
     if (!is.null(newdata)) {
         ## newdata should give variables in model formula
@@ -24,83 +29,44 @@ predict.glmmPQL <- function (object, newdata = NULL, newrandom = NULL, level = 1
         if (is.null(newrandom) || dim(newrandom) != c(np, ncol(object$random)))
             stop("newrandom should have ", np, " rows and ",
                  ncol(object$random), " columns")
+        browser()
         D <- cbind(D, newrandom)
         coef <- c(fixef(object), ranef(object))
         pred <- napredict(na.action, c(D %*% coef) + offset)
-        pred <- switch(type,
-                       "link" = pred,
-                       "response" = family(object)$linkinv(pred),
-                       "terms") ## need to fix
-
-        if (se.fit == TRUE) {
-            absorb <- function(D, c, n) {
-                e1 <- D[n, ]
-                e2 <- e1/c[n]
-                if (n > 1) tcrossprod(e1, e2) + Recall(D, c, n - 1)
-                else tcrossprod(e1, e2)
-            }
-            sigma <- object$sigma
-            X <- model.matrix(object)
-            Z <- object$random
-            w <- object$weights
-            c <- colSums(w * X^2)
-            c <- c(c, colSums(w * Z^2) + 1/sigma^2)
-            ## absorb -> diag(D %*% chol2inv(chol(C)) %*% t(D)) = var(eta)
-            se.pred <- sqrt(absorb(D, length(c)))
-            se.pred <- switch(type,
-                              "link" = se.pred,
-                              "response" = se.pred * abs(family(object)$mu.eta(pred)),
-                              "terms")
-            return(list(fit = pred, se.fit = se.pred))
-        }
-
-        return(pred)
+        if (type == "response")
+            pred <- family(object)$linkinv(pred)
     }
     else {
-        ## fitted values from model plus standard errors
-        ## D %*% coef + object$offset
-        na.act <- object$na.action
-        pred <- napredict(na.act, object$linear.predictors)
-        pred <- switch(type,
-                       "link" = pred,
-                       "response" = family(object)$linkinv(pred),
-                       "terms") ## need to fix
-
-        if (se.fit == TRUE) {
-            absorb <- function(D, C, n) {
-                f <- C[n, n]
-                e <- C[n, -n]
-                d <- D[n, ]
-                D <- D[-n, ] - tcrossprod(e/f, d)
-                C <- C[-n, -n] - tcrossprod(e/f, e)
-                if (n > 1) d^2/f + Recall(D, C, n - 1)
-                else  d^2/f
-            }
-            X <- model.matrix(object)
-            Z <- object$random
-            D <- cbind(X, Z)
-            sigma <- object$sigma
-            w <- object$weights
-            c <- colSums(w * X^2)
-            c <- c(c, colSums(w * Z^2) + 1/sigma^2)
-              wX <- sqrt(w) * X
-            wZ <- sqrt(w) * Z
-            XWX <- crossprod(wX)
-            XWZ <- crossprod(wX, wZ)
-            ZWZ <- crossprod(wZ, wZ)
-            diag(ZWZ) <- diag(ZWZ) + 1/sigma^2
-            C <- cbind(XWX, XWZ)
-            C <- rbind(C, cbind(t(XWZ), ZWZ))
-            browser()
-            ## absorb -> diag(D %*% chol2inv(chol(C)) %*% t(D)) = var(eta)
-            se.pred <- sqrt(absorb(t(D), C, length(c), nrow(D)))
-            se.pred <- switch(type,
-                              "link" = se.pred,
-                              "response" = se.pred * abs(family(object)$mu.eta(pred)),
-                              "terms")
-            return(list(fit = pred, se.fit = se.pred))
-        }
-
-        return(pred)
+        na.action <- object$na.action
+        pred <- napredict(na.action, object$linear.predictors)
+        if (type == "response")
+            pred <- family(object)$linkinv(pred)
     }
+    if (se.fit == TRUE) {
+        X <- model.matrix(object)
+        Z <- object$random
+        if (is.null(newdata)) D <- cbind(X, Z)
+        sigma <- object$sigma
+        w <- object$weights
+        wX <- sqrt(w) * X
+        wZ <- sqrt(w) * Z
+        XWX <- crossprod(wX)
+        XWZ <- crossprod(wX, wZ)
+        ZWZ <- crossprod(wZ, wZ)
+        diag(ZWZ) <- diag(ZWZ) + 1/sigma^2
+        C <- cbind(XWX, XWZ)
+        C <- rbind(C, cbind(t(XWZ), ZWZ))
+        ## diag(D %*% chol2inv(chol(C)) %*% t(D)) = var(eta)
+        D <- na.exclude(D)
+        H <- backsolve(chol(C), t(D), transpose = TRUE)
+        se.pred <- napredict(na.action,
+                             napredict(attr(D, "na.action"), sqrt(colSums(H^2))))
+        se.pred <- switch(type,
+                          "link" = se.pred,
+                          "response" = se.pred*abs(family(object)$mu.eta(pred)))
+        pred <- list(fit = pred, se.fit = se.pred)
+    }
+    if (0 %in% level)
+        list(population = lev0, individual = pred)
+    else pred
 }
