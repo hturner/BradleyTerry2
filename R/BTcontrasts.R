7eef9a99f246e6fd76a8bc3ea662f73d22e2ca55
Heather Turner <ht@heatherturner.net>
2019-02-24 14:21:13 +0000

    simplified BTcontrasts

--- R/BTcontrasts.R
+++ R/BTcontrasts.R
@@ -1,18 +1,12 @@
 # this can be used with e.g. car::lht; multcomp::glht; gmodels::estimable
-# use similar design to contrast::contrast for potential future extension
 BTcontrasts <- function(object, group1, group2, label = "grp1 v grp2"){
-    # replicate groups to match lengths
-    len_a <- lengths(group1)
-    len_b <- lengths(group2)
-    a <- rep(group1, each = len_b)
-    b <- rep(group2, len_a)
-    n <- length(a)
+    n <- length(group1)
     # set up contrast as if had full coef/vcov
     nm <- object$xlevels[[object$id]]
-    res <- matrix(0.0, n, length(nm), dimnames = list(label, nm))
+    res <- matrix(0.0, 1, length(nm), dimnames = list(label, nm))
     for (i in seq(n)){
-        res[1, a[[i]]] <- 1/length(a[[i]])
-        res[1, b[[i]]] <- -1/length(b[[i]])
+        res[1, group1] <- 1/length(group1)
+        res[1, group2] <- -1/length(group2)
     }
     # setup factor reflecting contrasts used ..
     fac <- factor(object$xlevels[[object$id]], 
