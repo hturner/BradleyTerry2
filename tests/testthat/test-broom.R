10647ed982489cd25dc59f3334c8fbde71f2ce5e
Heather Turner <ht@heatherturner.net>
2019-03-12 09:55:00 +0000

    update test-broom

--- tests/testthat/test-broom.R
+++ tests/testthat/test-broom.R
@@ -1,7 +1,7 @@
 # test compatibility with broom
 context("test-broom")
 
-test_that("Package compatible with broom", {
+test_that("augment works based on original data", {
     skip_on_cran()
     
     library(broom)
@@ -21,6 +21,12 @@ test_that("Package compatible with broom", {
     res2 <- augment(baseballModel1, type.predict = "ability")
     expect_known_output(res2, 
                         file = "outputs/augment.original.ability.rds")
+})
+ 
+test_that("augment works based on new data, standard BT", {   
+    skip_on_cran()
+    
+    library(broom)
     
     # set up test data
     test_teams <- c("Milwaukee", "Detroit")
@@ -44,5 +50,36 @@ test_that("Package compatible with broom", {
     res2 <- augment(baseballModel1, newdata = test[1], type.predict = "ability")
     expect_known_output(res2, 
                         file = "outputs/augment.new.ability.one.rds")
+    
+})
+
+test_that("augment works based on new data, with home advantage", {   
+    skip_on_cran()
+    
+    library(broom)
+    
+    baseballModel2 <- update(baseballModel1, 
+                             player1 = data.frame(team = baseball$home.team, 
+                                                  at.home = 1),
+                             player2 = data.frame(team = baseball$away.team, 
+                                                  at.home = 0),
+                             formula = ~ team + at.home)
+    
+    # set up test data
+    test_teams <- c("Milwaukee", "Detroit")
+    all_teams <- levels(baseball$home.team)
+    home.team <- data.frame(team = factor(test_teams, levels = all_teams),
+                            at.home = 1)
+    away.team <- data.frame(team = factor(rev(test_teams), levels = all_teams),
+                            at.home = 0)
+    
+    predict(baseballModel2, 
+            newdata = list(player1 = home.team, player2 = away.team))
+    
+    # Augment new contests with contest-level data
+    # doesn't work due to bug in broom 0.5.1
+    #res <- augment(baseballModel2, 
+    #               newdata = list(player1 = home.team, player2 = away.team),
+    #               type.predict = "link")
     
 })
