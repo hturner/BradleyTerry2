506f395eed0120c77c7753bf96c3336eb8101228
Heather Turner <ht@heatherturner.net>
2019-03-12 16:07:12 +0000

    add augment.BTm so can set level

---
+++ R/augment.BTm.R
@@ -0,0 +1,11 @@
+augment.BTm <- function(x, ...){
+    dots <- list(...)
+    if ("level" %in% names(dots)){
+        # reduce to fixed effects because level not passed on by augment.glm
+        if (dots$level == 0){
+            x$random <- NULL
+            x$sigma <- 0
+        }
+    }
+    NextMethod(x = x)
+}
